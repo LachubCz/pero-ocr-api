1696119fbc02f01402496f2a636892b9eb2d2c9b
LachubCz <petr.genius.buchal@gmail.com>
2020-09-07 12:59:48 +0200

    Updated database model.

--- app/db/model.py
+++ app/db/model.py
@@ -1,7 +1,9 @@
+import datetime
+
 from app.db import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy import Column, Enum, ForeignKey, Integer, String
+from sqlalchemy import Column, Enum, ForeignKey, Integer, String, DateTime
 from app.db.guid import GUID
 import enum
 from sqlalchemy.orm import relationship
@@ -17,22 +19,44 @@ class RequestState(enum.Enum):
     CANCELED = 'Request canceled.'
 
 
+class PageState(enum.Enum):
+    WAITING = 'Page is waiting for processing.'
+    PROCESSED = 'Page was processed.'
+
+
+class Permission(enum.Enum):
+    SUPER_USER = 'User can take and process requests.'
+    USER = 'User can create requests.'
+
+
 class ApiKey(Base):
     __tablename__ = 'api_key'
     __table_args__ = {'extend_existing': True}
     id = Column(Integer(), primary_key=True)
     api_string = Column(String(), nullable=False)
     owner = Column(String(), nullable=False)
-    permission = Column(Integer(), nullable=False)
+    permission = Column(Enum(Permission), nullable=False)
+
+    def __init__(self, api_string, owner, permission):
+        self.api_string = api_string
+        self.owner = owner
+        self.permission = permission
 
 
 class Request(Base):
     __tablename__ = 'request'
     __table_args__ = {'extend_existing': True}
     id = Column(GUID(), primary_key=True, default=uuid.uuid4)
-    state = Column(Enum(RequestState), nullable=False)
+    creation_timestamp = Column(DateTime(), nullable=False, default=datetime.datetime.utcnow)
+    modification_timestamp = Column(DateTime(), nullable=False, default=datetime.datetime.utcnow)
+    finish_timestamp = Column(DateTime(), nullable=True)
 
+    engine_id = Column(GUID(), ForeignKey('engine.id'), nullable=False)
     pages = relationship('Page', back_populates="request", lazy='dynamic')
+
+    def __init__(self, engine_id):
+        self.engine_id = engine_id
+
 
 class Page(Base):
     __tablename__ = 'page'
@@ -40,7 +64,17 @@ class Page(Base):
     id = Column(GUID(), primary_key=True, default=uuid.uuid4)
     name = Column(String(), nullable=False)
     path = Column(String(), nullable=False)
+    state = Column(Enum(PageState), nullable=False)
+    finish_timestamp = Column(DateTime(), nullable=True)
+
     request_id = Column(GUID(), ForeignKey('request.id'), nullable=False)
+    engine_version = Column(Integer(), ForeignKey('engine_version.id'), nullable=True)
+
+    def __init__(self, name, path, state, request_id):
+        self.name = name
+        self.path = path
+        self.state = state
+        self.request_id = request_id
 
 
 class Engine(Base):
@@ -48,8 +82,14 @@ class Engine(Base):
     __table_args__ = {'extend_existing': True}
     id = Column(Integer(), primary_key=True)
     name = Column(String(), nullable=False)
+    description = Column(String(), nullable=True)
 
     versions = relationship('EngineVersion', back_populates="engine", lazy='dynamic')
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
 
 class EngineVersion(Base):
     __tablename__ = 'engine_version'
@@ -57,6 +97,12 @@ class EngineVersion(Base):
     id = Column(Integer(), primary_key=True)
     version = Column(String(), nullable=False)
     engine_id = Column(Integer, ForeignKey('engine.id'), nullable=False)
+
+    pages = relationship('Page', back_populates="engine_version", lazy='dynamic')
+
+    def __init__(self, version, engine_id):
+        self.version = version
+        self.engine_id = engine_id
 
 
 if __name__ == '__main__':
