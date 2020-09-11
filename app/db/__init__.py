2792701e56427b7ccc12774a0f250519a74baf98
LachubCz <petr.genius.buchal@gmail.com>
2020-09-11 11:49:59 +0200

    Added new decorator for SUPER_USER.

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -2,5 +2,5 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-from .model import RequestState
+from .model import RequestState, Permission
 from .model import ApiKey, Request, Page, Engine, EngineVersion
--- app/db/api_key.py
+++ app/db/api_key.py
@@ -5,6 +5,7 @@ from functools import wraps
 from flask import request, abort
 
 from app.db import ApiKey
+from app.db import Permission
 
 
 def generate_hash_key():
@@ -15,7 +16,7 @@ def generate_hash_key():
                             random.choice(['rA', 'aZ', 'gQ', 'hH', 'hG', 'aR', 'DD']).encode('utf-8')).decode('utf-8').rstrip('==')
 
 
-def get_apiauth_object_by_key(key):
+def get_apiauth_object_by_key_u(key):
     """
     Query the datastorage for an API key.
     @return: apiauth sqlachemy object.
@@ -24,7 +25,16 @@ def get_apiauth_object_by_key(key):
     return api_string
 
 
-def match_api_keys(key):
+def get_apiauth_object_by_key_su(key):
+    """
+    Query the datastorage for an API key.
+    @return: apiauth sqlachemy object.
+    """
+    api_string = ApiKey.query.filter(ApiKey.api_string == key).filter(ApiKey.permission == Permission.SUPER_USER).first()
+    return api_string
+
+
+def match_api_keys(key, permission):
     """
     Match API keys
     @param key: API key from request
@@ -32,20 +42,39 @@ def match_api_keys(key):
     """
     if key is None:
         return False
-    api_key = get_apiauth_object_by_key(key)
+
+    if permission == Permission.USER:
+        api_key = get_apiauth_object_by_key_u(key)
+    elif permission == Permission.SUPER_USER:
+        api_key = get_apiauth_object_by_key_su(key)
+
     if api_key is not None:
         return True
     return False
 
 
-def require_app_key(f):
+def require_super_user_api_key(f):
     """
     @param f: flask function
     @return: decorator, return the wrapped function or abort json object.
     """
     @wraps(f)
     def decorated(*args, **kwargs):
-        if match_api_keys(request.headers.get('api-key')):
+        if match_api_keys(request.headers.get('api-key'), Permission.SUPER_USER):
+            return f(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated
+
+
+def require_user_api_key(f):
+    """
+    @param f: flask function
+    @return: decorator, return the wrapped function or abort json object.
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if match_api_keys(request.headers.get('api-key'), Permission.USER):
             return f(*args, **kwargs)
         else:
             abort(401)
